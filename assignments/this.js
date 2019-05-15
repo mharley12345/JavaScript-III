2b6352171ee18ffe80271f5df6e734690a377954
mharley12345 <49734550+mharely12345@users.noreply.github.com>
2019-05-15 15:18:43 -0700

    n/a

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,71 @@
 /* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
-*
-* 1. 
-* 2. 
-* 3. 
-* 4. 
-*
-* write out a code example of each explanation above
-*/
+ * in your own words. explain the four principle for the "this" keyword below.
+ *
+ * 1. Using call or apply you can refer `${this}` to any abject
+ * 2. `This` can be undefined using 'strict'
+ * 3. It can be used global
+ * 4. It can be bound to another function keeping same argument from first function.
+ *
+ * write out a code example of each explanation above
+ */
 
 // Principle 1
 
 // code example for Window Binding
 
-// Principle 2
+//  window.setInterval(function(time){
+//    time=1000
+//    console.log("It Has Been 10 Seconds")   
+//   window.setInterval(30)
+//  })
+       
+//  return time
+ 
+ // function wait(){
+        //   for(i=0;i>=10;i++)
+        //   setInterval(function() {
+        //     alert("Message to alert every 5 seconds");
+        // }, 5000);
+          
 
-// code example for Implicit Binding
+    
+    
 
-// Principle 3
 
-// code example for New Binding
 
-// Principle 4
 
-// code example for Explicit Binding
+
+
+
+
+
+
+
+
+      // Principle 2
+
+      // code example for Implicit Binding
+          var Mike = {
+            name:'Mike',
+            says: function(message){
+           console.log( 'name'+'says
+              }
+                
+      
+        
+            
+           
+         
+      
+      
+         
+
+
+      // Principle 3
+
+      // code example for New Binding
+
+
+      // Principle 4
+
+      // code example for Explicit Binding
