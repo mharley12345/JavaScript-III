ab9fabd0f7079ef3640ae32829faa107c8a21255
mharley12345 <49734550+mharely12345@users.noreply.github.com>
2019-05-15 18:26:00 -0700

    N/A

--- assignments/this.js
+++ assignments/this.js
@@ -11,25 +11,21 @@
 
 // Principle 1
 
-// code example for Window Binding
+// code example for Window Binding        
 
 //  window.setInterval(function(time){
 //    time=1000
 //    console.log("It Has Been 10 Seconds")   
 //   window.setInterval(30)
 //  })
-       
+
 //  return time
- 
- // function wait(){
-        //   for(i=0;i>=10;i++)
-        //   setInterval(function() {
-        //     alert("Message to alert every 5 seconds");
-        // }, 5000);
-          
 
-    
-    
+// function wait(){                      
+//   for(i=0;i>=10;i++)           
+//   setInterval(function() {
+//     alert("Message to alert every 5 seconds");
+// }, 5000);                      
 
 
 
@@ -42,30 +38,35 @@
 
 
 
-      // Principle 2
 
-      // code example for Implicit Binding
-          var Mike = {
-            name:'Mike',
-            says: function(message){
-           console.log( 'name'+'says
-              }
-                
-      
-        
-            
-           
-         
-      
-      
-         
+// Principle 2                      
+
+// code example for Implicit Binding
 
 
-      // Principle 3
+var MyObject = function () {
+    this.name = 'MyObjectName';
+    this.myProperty = 'property';
 
-      // code example for New Binding
 
 
-      // Principle 4
+};
 
-      // code example for Explicit Binding
+   MyObject.prototype.doStuff = function (action){
+       console.log(this.name + ' is ' + action +'!');
+   }
+     var obj = new MyObject();
+     obj.doStuff('awesome');
+
+// Not my functon. I got it off the internet
+
+
+
+// Principle 3
+
+// code example for New Binding
+
+
+// Principle 4
+
+// code example for Explicit Binding
